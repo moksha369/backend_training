e1702eb6cbe825b26922ea6a73aaccb536031d4d
moksha369 <iesbsamuel@gmail.com>
2025-09-16 21:46:56 -0300

    Aula06 IMPORTANTE.

--- Aulas/aula06/app.js
+++ Aulas/aula06/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+
 const indexRouter = require('./routes/index');
 
 const app = express();
@@ -14,4 +15,26 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 
+const tarefas = [];
+app.get('/tarefas', (req, res) => {
+    res.json(tarefas);
+});
+
+app.post('/tarefas', (req, res) => {
+    const novaTarefa = {
+        ...req.body,
+        id: tarefas.length + 1
+    };
+    tarefas.push(novaTarefa)
+    res.status(201).json(novaTarefa);
+});
+
+app.get("/tarefas/:id", (req, res) => {
+    const { id } = req.params;
+    const tarefaEncontrada = tarefas.find((item) => 
+        item.id == parseInt(id));
+    if (tarefaEncontrada)
+        return res.json(tarefaEncontrada);
+    res.status(404).json({msg: "Tarefa não encontrada."})
+ });
 module.exports = app;
